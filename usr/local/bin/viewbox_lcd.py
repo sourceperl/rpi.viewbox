59c0f54e6a9d584e45715bbfcce76eaae5fb588d
sourceperl <loic.celine@free.fr>
2022-06-26 20:57:47 +0200

    update LCD script

--- usr/local/bin/viewbox_lcd.py
+++ usr/local/bin/viewbox_lcd.py
@@ -2,118 +2,102 @@
 
 import time
 import json
-import threading
+from typing import Any
 import schedule
 import paho.mqtt.client as mqtt
 import RPi_I2C_LCD
 
+
+# some const
+VIG_LVL = {1: 'V', 2: 'J', 3: 'O', 4: 'R'}
+
+
+# some class
+class ValueUpd:
+    """A value with an up-to-date flag."""
+
+    def __init__(self, value: Any = None, ttl: int = 3600) -> None:
+        # public
+        self.ttl = ttl
+        # private
+        self._value = None
+        self._update_t = 0
+        # properties
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value is not None:
+            self._value = value
+            self._update_t = time.monotonic()
+
+    @property
+    def up(self) -> bool:
+        return self._update_t + self.ttl > time.monotonic()
+
+
 # global vars
-vig_level = {
-    1: 'V',
-    2: 'J',
-    3: 'O',
-    4: 'R',
-}
-vig_59 = 'I'
-vig_59_up = 0
-vig_62 = 'I'
-vig_62_up = 0
-p_atmo = 0.0
-p_atmo_up = 0
-t_atmo = 0.0
-t_atmo_up = 0
-nb_mail = 0
-nb_mail_up = 0
+vig_59 = ValueUpd('I', ttl=3600)
+vig_62 = ValueUpd('I', ttl=3600)
+p_atmo = ValueUpd(0.0, ttl=3600)
+t_atmo = ValueUpd(0.0, ttl=3600)
+nb_mail = ValueUpd(0, ttl=900)
 
 
-def on_connect(client, userdata, flags, rc):
-    print('Connected with result code ' + str(rc))
-    client.subscribe('pub/meteo_vig/dep/59')
-    client.subscribe('pub/meteo_vig/dep/62')
-    client.subscribe('pub/house/garden/pressure_sea_level')
-    client.subscribe('pub/house/garden/temperature')
-    client.subscribe('pub/mail/loic.celine/unread')
+class CustomMQTT(mqtt.Client):
+    def on_connect(self, client, userdata, flags, rc):
+        # subscribe to topics here for redo after disconnect
+        client.subscribe('pub/meteo_vig/dep/59')
+        client.subscribe('pub/meteo_vig/dep/62')
+        client.subscribe('pub/metars/lesquin')
+        client.subscribe('pub/mail/loic.celine/unread')
 
-
-def on_disconnect(client, userdata, rc):
-    print('MQTT disconnect')
-
-
-def on_message(client, userdata, msg):
-    # global vars
-    global vig_59, vig_59_up
-    global vig_62, vig_62_up
-    global p_atmo, p_atmo_up
-    global t_atmo, t_atmo_up
-    global nb_mail, nb_mail_up
-    # process topic
-    if msg.topic == 'pub/meteo_vig/dep/59':
+    def on_message(self, client, userdata, msg):
+        # global vars
+        global vig_59, vig_62, p_atmo, t_atmo, nb_mail
+        # try to update values
         try:
-            index = int(json.loads(msg.payload.decode())['value'])
-            vig_59 = vig_level[index]
-            vig_59_up = int(time.time())
-        except:
-            pass
-    elif msg.topic == 'pub/meteo_vig/dep/62':
-        try:
-            index = int(json.loads(msg.payload.decode())['value'])
-            vig_62 = vig_level[index]
-            vig_62_up = int(time.time())
-        except:
-            pass
-    elif msg.topic == 'pub/house/garden/pressure_sea_level':
-        try:
-            p_atmo = float(json.loads(msg.payload.decode())['value'])
-            p_atmo_up = int(time.time())
-        except:
-            pass
-    elif msg.topic == 'pub/house/garden/temperature':
-        try:
-            t_atmo = float(json.loads(msg.payload.decode())['value'])
-            t_atmo_up = int(time.time())
-        except:
-            pass
-    elif msg.topic == 'pub/mail/loic.celine/unread':
-        try:
-            nb_mail = int(json.loads(msg.payload.decode())['value'])
-            nb_mail_up = int(time.time())
+            # process payload (json -> dict)
+            msg_pld_d = json.loads(msg.payload.decode())
+            # process topics
+            if msg.topic == 'pub/meteo_vig/dep/59':
+                vig_59.value = VIG_LVL.get(msg_pld_d['value'], None)
+            elif msg.topic == 'pub/meteo_vig/dep/62':
+                vig_62.value = VIG_LVL.get(msg_pld_d['value'], None)
+            elif msg.topic == 'pub/metars/lesquin':
+                t_atmo.value = float(msg_pld_d['temp'])
+                p_atmo.value = float(msg_pld_d['press'])
+            elif msg.topic == 'pub/mail/loic.celine/unread':
+                nb_mail.value = int(msg_pld_d['value'])
         except:
             pass
 
 
 def lcd_job():
     # global vars
-    global vig_59, vig_59_up
-    global vig_62, vig_62_up
-    global p_atmo, p_atmo_up
-    global t_atmo, t_atmo_up
-    global nb_mail, nb_mail_up
-    # format and display
+    global vig_59, vig_62, p_atmo, t_atmo, nb_mail
+    # format vars for display
     now = int(time.time())
-    # l1
-    p_atmo_str = '%7.2f hPa' % p_atmo if (now - p_atmo_up < 360) else '      ? hPa'
-    vig_59 = vig_59 if (now - vig_59_up < 3600) else str('?')
-    line1 = str('%s     59:%c' % (p_atmo_str, vig_59)).ljust(20)[:20]
-    # l2
-    t_atmo_str = '%7.2f C' % t_atmo if (now - t_atmo_up < 360) else '      ? C'
-    vig_62 = vig_62 if (now - vig_62_up < 3600) else str('?')
-    line2 = str('%s       62:%c' % (t_atmo_str, vig_62)).ljust(20)[:20]
-    # l3
-    str_mail = str(nb_mail) if (now - nb_mail_up < 900) else '?'
-    line3 = '%7s %s' % (str_mail, 'e-mails' if (nb_mail > 1) else 'e-mail')
-    line3 = line3.ljust(20)[:20]
-    # l4
-    datetime = time.strftime('%d/%m/%Y  %H:%M:%S', time.localtime())
-    line4 = (datetime).ljust(20)[:20]
+    p_atmo_lcd = f'{p_atmo.value:7.2f}' if p_atmo.up else '?'
+    t_atmo_lcd = f'{t_atmo.value:7.2f}' if t_atmo.up else '?'
+    vig_59_lcd = vig_59.value if vig_59.up else '?'
+    vig_62_lcd = vig_62.value if vig_62.up else '?'
+    mail_nb_lcd = f'{nb_mail.value}' if nb_mail.up else '?'
+    mail_lbl_lcd = 'e-mails' if nb_mail.value > 1 else 'e-mail'
     # send result to LCD
     lcd.set_cursor(row=0)
-    lcd.message(line1)
+    lcd.message(f'{p_atmo_lcd:>7} hPa     59:{vig_59_lcd}')
     lcd.set_cursor(row=1)
-    lcd.message(line2)
+    lcd.message(f'{t_atmo_lcd:>7} C       62:{vig_62_lcd}')
     lcd.set_cursor(row=2)
-    lcd.message(line3)
+    lcd.message(f'{mail_nb_lcd:>7} {mail_lbl_lcd}')
     lcd.set_cursor(row=3)
-    lcd.message(line4)
+    lcd.message(time.strftime('%d/%m/%Y  %H:%M:%S', time.localtime()))
+
 
 if __name__ == '__main__':
     # init LCD screen
@@ -122,16 +106,19 @@ if __name__ == '__main__':
     lcd.home()
     lcd.message('connect MQTT server')
     # init MQTT client
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_message = on_message
-    client.connect('192.168.1.60', port=1883, keepalive=30)
+    client = CustomMQTT()
+    # connect try loop
+    while True:
+        try:
+            client.connect('192.168.1.60', port=1883, keepalive=30)
+            break
+        except OSError:
+            time.sleep(5.0)
+    # start MQTT I/O thread
     client.loop_start()
-    # schedule conf.
+    # int scheduler
     schedule.every(.5).seconds.do(lcd_job)
     # main loop
     while True:
         schedule.run_pending()
         time.sleep(.1)
-
